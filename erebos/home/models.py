c03856fa3188f6051076e31d10b9b8e0f9639cc3
kleberbaum <kleberbaum@erebos.xyz>
2017-11-19 17:52:57 +0100

    replaced some of the dumb code with smart code

--- erebos/home/models.py
+++ erebos/home/models.py
@@ -5,62 +5,85 @@ from django.db import models
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import StreamField, RichTextField
 from wagtail.wagtailcore import blocks
-from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.wagtailadmin.edit_handlers import TabbedInterface, ObjectList, InlinePanel, StreamFieldPanel, MultiFieldPanel, FieldPanel
 from wagtail.wagtailimages.blocks import ImageChooserBlock
 
 
 class HomePage(Page):
-    modt = models.CharField(max_length=16, default="")
+    header = StreamField([
+        ('hbanner', blocks.StructBlock([
+            ('banner', blocks.CharBlock(blank=True, classname="full title", icon='title'))
+        ], required=False, icon='bold')),
 
-    sh1 = RichTextField()
-    sh2 = RichTextField()
-    sh3 = RichTextField()
+        ('hfull', blocks.StructBlock([
+            ('full', blocks.CharBlock(blank=True, classname="full title", icon='title'))
+        ], required=False, icon='placeholder')),
 
-    about1 =  StreamField([
-        ('title', blocks.CharBlock(classname="full title")),
-        ('content', blocks.RichTextBlock()),
+        ('hcode', blocks.StructBlock([
+            ('code', blocks.RawHTMLBlock(blank=True, classname="full"))
+        ], icon='code'))
+    ], blank=True)
+
+    article = StreamField([
+        ('aabout', blocks.StructBlock([
+            ('about_pages', blocks.StreamBlock([
+                ('about', blocks.StructBlock([
+                    ('blink', blocks.CharBlock(blank=True, classname="full")),
+                    ('use_image', blocks.BooleanBlock(default=False, help_text="Use picture instead of blink", required=False, classname="full")),
+                    ('image', ImageChooserBlock(required=False, classname="full")),
+                    ('boxes', blocks.StreamBlock([
+                        ('title', blocks.CharBlock(blank=True, classname="full title", icon='title')),
+                        ('content', blocks.RichTextBlock(blank=True, classname="full"))
+                    ]))
+                ], icon='doc-full'))
+            ], icon='cogs')),
+        ], icon='radio-empty')),
+
+        ('amotd', blocks.StructBlock([
+            ('modt', blocks.CharBlock(max_length=16, default="Sky's The Limit", classname="full")),
+        ], icon='pilcrow')),
+
+        ('asharingan', blocks.StructBlock([
+            ('sharingan', blocks.StructBlock([
+                ('sharingan_1', blocks.RichTextBlock(default="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", classname="full")),
+                ('sharingan_2', blocks.RichTextBlock(default="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", classname="full")),
+                ('sharingan_3', blocks.RichTextBlock(default="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", classname="full")),
+
+                ('nyan_titel', blocks.CharBlock(max_length=16, default="The Team", classname="full")),
+                ('show_team', blocks.BooleanBlock(default=False, help_text="Whether team block will be shown on page", required=False, classname="full")),
+                ('team', blocks.StreamBlock([
+                    ('member', blocks.StructBlock([
+                        ('pic', ImageChooserBlock(blank=True, classname="full")),
+                        ('name', blocks.CharBlock(blank=True, max_length=16, default="", classname="full")),
+                        ('description', blocks.CharBlock(max_length=128, default="", classname="full"))
+                    ], icon='user'))
+                ], required=False))
+            ]))
+        ], icon='view')),
+
+        ('aspaceship', blocks.StructBlock([
+        ], icon='pick')),
+
+        ('agallery', blocks.StructBlock([
+            ('title', blocks.CharBlock(blank=True, classname="full")),
+            ('gallery', blocks.StreamBlock([
+                ('image', ImageChooserBlock(blank=True, classname="full")),
+            ]))
+        ], icon='grip')),
+
+        ('acode', blocks.StructBlock([
+            ('code', blocks.RawHTMLBlock(blank=True, classname="full"))
+        ], icon='code'))
+    ], blank=True)
+
+
+
+    main_content_panels = [
+        StreamFieldPanel('header'),
+        StreamFieldPanel('article')
+    ]
+
+    edit_handler = TabbedInterface([
+        ObjectList(Page.content_panels + main_content_panels, heading='Main'),
+        ObjectList(Page.promote_panels + Page.settings_panels, heading='Settings', classname="settings")
     ])
-
-    about2 =  StreamField([
-        ('title', blocks.CharBlock(classname="full title")),
-        ('content', blocks.RichTextBlock()),
-    ])
-
-    about3 =  StreamField([
-        ('title', blocks.CharBlock(classname="full title")),
-        ('content', blocks.RichTextBlock()),
-    ])
-
-    about4 =  StreamField([
-        ('title', blocks.CharBlock(classname="full title")),
-        ('content', blocks.RichTextBlock()),
-    ])
-
-    about5 =  StreamField([
-        ('title', blocks.CharBlock(classname="full title")),
-        ('content', blocks.RichTextBlock()),
-    ])
-
-    about6 =  StreamField([
-        ('title', blocks.CharBlock(classname="full title")),
-        ('content', blocks.RichTextBlock()),
-    ])
-
-    nyan = models.CharField(max_length=16, default="")
-
-    content_panels = Page.content_panels + [
-            FieldPanel('modt', classname="full"),
-
-            StreamFieldPanel('about1'),
-            StreamFieldPanel('about2'),
-            StreamFieldPanel('about3'),
-            StreamFieldPanel('about4'),
-            StreamFieldPanel('about5'),
-            StreamFieldPanel('about6'),
-
-            FieldPanel('sh1', classname="full"),
-            FieldPanel('sh2', classname="full"),
-            FieldPanel('sh3', classname="full"),
-
-            FieldPanel('nyan', classname="full"),
-        ]
